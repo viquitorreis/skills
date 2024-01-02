f061b8dc49d2e5947545ad5521c70eabaa3d13a6
victor <victor@iuptec.com.br>
2024-01-02 14:01:49 -0300

    commit #10 - comecando dash admin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [
@@ -9,7 +11,9 @@ import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.compo
     AdminDashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CardModule,
+    MenubarModule
   ]
 })
 export class AdminModule { }
